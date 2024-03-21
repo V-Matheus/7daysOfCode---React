5a0267f8457d9fc811a70c3b2e2e47ebe59f65c8
Victor Matheus <victormatheus507@gmail.com>
2024-03-21 09:17:55 -0300

    criando grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
-import background from './assets/Vector.png'
+import background from './assets/Vector.png';
 
 import Header from './components/Header/Header';
-import './styles/styles.scss'
+import './styles/styles.scss';
 
 function App() {
-
   return (
     <>
-      <img className='background' src={background} alt="" />
-      <Header/>
+      <main>
+        <img className="background" src={background} alt="" />
+        <Header />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
