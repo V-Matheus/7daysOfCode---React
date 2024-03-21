76bca752b8107ea95cf5ee407bdcea13333a568c
Victor Matheus <victormatheus507@gmail.com>
2024-03-21 12:28:38 -0300

    tipografia do header

--- src/components/Header/HeaderStyled.tsx
+++ src/components/Header/HeaderStyled.tsx
@@ -5,6 +5,12 @@ export const HeaderStyled = styled.div`
   grid-column: 3 / 5;
   grid-row: 1;
 
+  font-family: Montserrat;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 19.5px;
+  text-align: left;
+
   nav {
     width: 100%;
     margin: auto;
