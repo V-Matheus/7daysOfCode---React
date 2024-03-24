e8e86f1459b0cc64cdc1f215265fe8af3cc7f366
Victor Matheus <victormatheus507@gmail.com>
2024-03-24 11:07:40 -0300

    cards ofertas

--- src/components/ofertas/Ofertas.tsx
+++ src/components/ofertas/Ofertas.tsx
@@ -2,29 +2,85 @@ import React from 'react';
 import { Container } from './OfertasStyled';
 import seta from '../../assets/seta.svg';
 
+import produto1 from '../../assets/produto-01.png';
+import produto2 from '../../assets/produto-02.png';
+import produto3 from '../../assets/produto-03.png';
+import produto4 from '../../assets/produto-04.png';
+import produto5 from '../../assets/produto-05.png';
+import produto6 from '../../assets/produto-06.png';
+
+type Ofertas = {
+  id: number;
+  foto: string;
+  titulo: string;
+  preco: number;
+};
+
+const ofertas: Ofertas[] = [
+  {
+    id: 1,
+    foto: produto1,
+    titulo: 'Ajuga reptans',
+    preco: 20,
+  },
+  {
+    id: 2,
+    foto: produto2,
+    titulo: 'Cordyline fruticosa',
+    preco: 20,
+  },
+  {
+    id: 3,
+    foto: produto3,
+    titulo: 'Crassula ovata',
+    preco: 20,
+  },
+  {
+    id: 4,
+    foto: produto4,
+    titulo: 'Cyperus rotundus',
+    preco: 20,
+  },
+  {
+    id: 5,
+    foto: produto5,
+    titulo: 'Delairea odorata',
+    preco: 20,
+  },
+  {
+    id: 6,
+    foto: produto6,
+    titulo: 'Datura metel',
+    preco: 20,
+  },
+];
+
 const Ofertas = () => {
   return (
     <Container>
       <div>
-        <div className='ofertasTittle'>
+        <div className="ofertasTittle">
           <span>Conheça nossas</span>
           <h1>ofertas</h1>
         </div>
 
-        <div className='ofertasContainer'>
-          <div className="oferta">
-            <div className="ofertaImg">
-              <img src="" alt="" />
-            </div>
-            <div className='ofertaContent'>
-              <h1>Ajuga reptans</h1>
-              <span>R$ 20,00</span>
-              <div className='ofertaButton'>
-                <a href="#">Comprar</a>
-                <img src={seta} alt="" />
+        <div className="ofertasContainer">
+          {ofertas.map((oferta) => (
+            <div className="oferta sombra" key={oferta.id}>
+              <div className="ofertaImg">
+                <img src={oferta.foto} alt="" />
+              </div>
+              <div className="ofertaContent">
+                <h1 className="titulo">{oferta.titulo}</h1>
+                <span className="subtitulo">
+                  R$ {oferta.preco.toFixed(2).replace('.', ',')}
+                </span>
+                <div className="ofertaButton">
+                  <a href="#">Comprar <img src={seta} alt="" /></a>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </Container>
--- src/components/ofertas/OfertasStyled.tsx
+++ src/components/ofertas/OfertasStyled.tsx
@@ -1,6 +1,53 @@
 import { styled } from 'styled-components';
+import { colorPrimary } from '../../styles/stylesGlobais';
 
 export const Container = styled.div`
   grid-column: 2/ 5;
   grid-row: 5;
+
+  .ofertasContainer {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 25px;
+  }
+
+  .oferta {
+    display: flex;
+    text-align: center;
+    width: calc(33.33% - 30px);
+    overflow-y: hidden;
+
+    h1 {
+      font-family: Elsie Swash Caps;
+      font-size: 32px;
+      font-weight: 900;
+      line-height: 36.86px;
+      text-align: left;
+    }
+  }
+
+  .ofertaImg {
+    display: flex;
+    scale: 1.2;
+  }
+
+  .ofertaContent {
+    padding: 15px;
+    display: flex;
+    justify-content: center;
+    text-align: left;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .ofertaButton a {
+    color: ${colorPrimary};
+    display: inline-block;
+    padding: 10px 0;
+
+    img {
+      margin-left: 20px;
+      width: 14px;
+    }
+  }
 `;
